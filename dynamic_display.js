e70ca053c06f247bb28c4c3c8e0d835d45a1af80
KAKANI SASI <125825878+KAKANISASI@users.noreply.github.com>
2023-04-30 12:43:21 +0530

    Add files via upload
    There are two major types of Javascript, and this Udacity Javascript hub covers both. The first type is “vanilla” Javascript, scripts you can write using the basic rules in the Javascript standard. The second type is framework-driven Javascript that creates complex content building upon others’ frameworks, like Angular and React or libraries like FusionCharts.
    
    This Javascript hub contains easy-to-follow guides on a wide range of Javascript topics. Most articles provide sample code to demonstrate how Javascript works in the real world.
    
    This hub is meant to be a comprehensive resource, so we will add new content as it’s published. Check back here for more Javascript content.

---
+++ dynamic_display.js
@@ -0,0 +1,45 @@
+dynamic_display.js
+
+function ModifyBGColor(id, newColor)
+
+{
+
+ var mElement = document.getElementById(id);
+
+ mElement.style.backgroundColor = newColor;
+
+}
+
+function ModifyTextColor(id, newColor)
+
+{
+
+ var mElement = document.getElementById(id);
+
+ mElement.style.color = newColor;
+
+}
+
+function ModifyBoxSize(id, newWidth, newHeight)
+
+{
+
+ var mElement = document.getElementById(id);
+
+ mElement.style.width = newWidth;
+
+ mElement.style.height = newheight;
+
+}
+
+function ModifyBoxPosition(id, newLeft, newTop)
+
+{
+
+ var mElement = document.getElementById(id);
+
+ mElement.style.left = newLeft;
+
+ mElement.style.top = newTop;
+
+}
